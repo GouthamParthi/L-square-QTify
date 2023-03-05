38629d5a5cee996bb52431d92b1cb5f0acef8f2f
GouthamParthi <goutham.parthi444@gmail.com>
2023-03-05 16:54:50 +0530

    C3:SearchBar

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-
+import SearchIcon from "@mui/icons-material/Search";
 import styles from "./Search.module.css";
 function Search() {
   return (
     <div>
       <div className={styles.container}>
-        <input type="text" className={styles.search} />
-        <button className={styles.searchButton}>Search</button>
+        <input
+          type="text"
+          className={styles.search}
+          placeholder="Search a song of your choice"
+        />
+        <button className={styles.searchButton}>
+          <SearchIcon />
+        </button>
       </div>
     </div>
   );

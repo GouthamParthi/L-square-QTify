c19da56298606f4fc6e202e6f07872d8710b76c2
GouthamParthi <goutham.parthi444@gmail.com>
2023-03-05 16:28:19 +0530

    C3:SearchBar

---
+++ qtify/src/components/GiveFeedbackButton/GiveFeedbackButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./GiveFeedbackButton.module.css";
+
+function GiveFeedbackButton() {
+  return (
+    <div>
+      <button className={styles.giveFeedback}>Give Feedback</button>
+    </div>
+  );
+}
+
+export default GiveFeedbackButton;

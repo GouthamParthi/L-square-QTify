361adc5f048b90e12268098a2746a8bbdd30d015
GouthamParthi <goutham.parthi444@gmail.com>
2023-03-05 16:38:27 +0530

    C3:SearchBar

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./Logo.module.css";
 const logo = require("../../assets/Logo.png");
 function Logo() {
   return (
     <div>
-      <img src={logo} alt="logo" width={67} height={34} />
+      <img
+        className={styles.logo}
+        src={logo}
+        alt="logo"
+        width={67}
+        height={34}
+      />
     </div>
   );
 }

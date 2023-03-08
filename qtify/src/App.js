f26cf5b24ed5ac37042b7da4740a0fa0e4230528
GouthamParthi <goutham.parthi444@gmail.com>
2023-03-08 17:11:09 +0530

    S1:hero-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/Navabar/Navbar";
-
+import HeroSection from "./components/HeroSection/HeroSection";
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
---
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,21 @@
+import { Headphones } from "@mui/icons-material";
+import React from "react";
+import styles from "./HeroSection.module.css";
+const headphone = require("../../assets/vibrating-headphone.png");
+function HeroSection() {
+  return (
+    <div className={styles.heroSection}>
+      <section className={styles.container}>
+        <p>100 Thousand Songs, ad-free</p>
+        <p>Over thousands podcast episodes</p>
+      </section>
+      <img
+        src={headphone}
+        className={styles.headphonesImage}
+        alt="vibratingHeadphone"
+      />
+    </div>
+  );
+}
+
+export default HeroSection;
